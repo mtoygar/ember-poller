6b97ebd3cf5a9d5be6fca771823809837fef078c
Murat Toygar <toygar-murat@hotmail.com>
2019-06-30 21:05:43 +0300

    upgrade ember to 3.10.1
    to be comply with ember 3.10.1 ember-concurrency is also upgraded and saved to dev dependencies

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
   name: 'ember-poller',
   included: function(app) {
+  	this._super.included.apply(this, arguments);
     app.import('node_modules/normalize.css/normalize.css');
   }
 };
