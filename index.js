12ce31f328986e985823cef80cff3815e4337544
Murat Toygar <toygar-murat@hotmail.com>
2019-07-01 21:51:25 +0300

    revert ember-update changes

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = {
   name: 'ember-poller',
   included: function(app) {
-  	this._super.included.apply(this, arguments);
     app.import('node_modules/normalize.css/normalize.css');
   }
 };
