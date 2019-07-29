fd3dcbb8d8a2237b80e405cba92527ebfafe309d
Scott Kidder <scott.kidder@ilq.com>
2019-07-29 11:39:58 -0400

    chore: don't force host apps to import normalize.css - its only needed for dummy app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,5 +14,7 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
+  app.import('node_modules/normalize.css/normalize.css');
+
   return app.toTree();
 };
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-poller',
-  included: function(app) {
-    app.import('node_modules/normalize.css/normalize.css');
-  }
+  name: 'ember-poller'
 };
